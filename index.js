19390ac09ea29359af93b7f6133816f09315f4e2
HexCipher224 <mofiamadiback@gmail.com>
2026-04-16 13:11:11 +0300

    initial commit

--- index.js
+++ index.js
@@ -1,35 +1,29 @@
 // Write your code here!
-// Display function
+
 function displayPosts(posts) {
     const postList = document.getElementById("post-list");
-
+    postList.innerHTML = "";
     posts.forEach(post => {
-    const li = document.createElement("li");
+        const li = document.createElement("li");
+        const h1 = document.createElement("h1");
+            h1.textContent = post.title;
+        const p = document.createElement("p");
+            p.textContent = post.body;
 
-    const h1 = document.createElement("h1");
-    h1.textContent = post.title;
+            li.appendChild(h1);
+            li.appendChild(p);
 
-    const p = document.createElement("p");
-    p.textContent = post.body;
-
-    li.appendChild(h1);
-    li.appendChild(p);
-
-    postList.appendChild(li);
-});
+            postList.appendChild(li);
+    });
 }
-
-// Async fetch function
 async function fetchPosts() {
-try {
-    const response = await fetch("https://jsonplaceholder.typicode.com/posts");
-    const posts = await response.json();
-
-    displayPosts(posts); // VERY IMPORTANT
-} catch (error) {
-    console.error(error);
+    try {
+        const response = await fetch ("https://jsonplaceholder.typicode.com/posts");
+        const posts = await response.json();
+        return displayPosts(posts);
+    } catch (error) {
+        console.error("Error fetching posts:", error);
+    }
 }
-}
 
-// Call function
 fetchPosts();
